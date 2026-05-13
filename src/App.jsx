767de219d545811a34678999ab4b2efa807be04b
ginkapetrova <ginkakuzmanova@gmail.com>
2026-05-13 21:00:11 +0300

    Clean up boilerplate,add assets

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const App = () => {
+    return (
+        <main>App</main>
+    )
+}
+export default App
