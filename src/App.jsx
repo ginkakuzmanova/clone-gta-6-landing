c17cb6d5cec3e416b1fe884c54ffa392543d26b9
ginkapetrova <ginkakuzmanova@gmail.com>
2026-05-15 11:20:29 +0300

    implement hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import gsap from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Hero from "./sections/Hero.jsx";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
-        <main>App</main>
+        <main>
+            <nav>
+                <img src="/images/nav-logo.svg" alt="logo" className={'scale-90'}/>
+                <img src="/images/menu.svg" alt="menu" className={'w-10'}/>
+            </nav>
+            <Hero />
+        </main>
     )
 }
 export default App
